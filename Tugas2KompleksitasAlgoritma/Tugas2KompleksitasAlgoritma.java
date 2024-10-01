54f5f76fd0175d6ad8905caaaafda6f9957568f8
SALMANAF76 <161696591+SALMANAF76@users.noreply.github.com>
2024-10-01 20:31:16 +0700

    Add files via upload

---
+++ Tugas2KompleksitasAlgoritma/Tugas2KompleksitasAlgoritma.java
@@ -0,0 +1,12 @@
+public class Tugas2KompleksitasAlgoritma {
+    public static void main(String[] args) {
+        int bilangan = 20;
+        int hasil = 0;
+
+        for (int i = 0; i < 2; i++) {
+            hasil += bilangan;
+        }
+        
+        System.out.println("Hasil dari " + bilangan + " pangkat dua adalah: " + hasil);
+    }
+}
